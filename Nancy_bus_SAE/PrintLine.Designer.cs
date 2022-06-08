b781254cf8ca8bda4a377486b8cc605ddf04044c
Florian <escande.florian.chatco@gmail.com>
2022-06-08 20:26:58 +0200

    Final commit center screen

--- Nancy_bus_SAE/PrintLine.Designer.cs
+++ Nancy_bus_SAE/PrintLine.Designer.cs
@@ -107,6 +107,7 @@
             this.Controls.Add(this.lblStop);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "PrintLine";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Affichage Lignes";
             this.ResumeLayout(false);
             this.PerformLayout();
